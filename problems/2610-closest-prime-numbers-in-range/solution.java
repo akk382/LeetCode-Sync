47abc01ecde6cdbff769efe5b545fcd1cf95cd89
Arun <arunyoyo00@gmail.com>
2025-03-08 09:11:54 +0000

    [LeetCode Sync] Runtime - 82 ms (79.37%), Memory - 44.7 MB (71.03%)

--- problems/2610-closest-prime-numbers-in-range/solution.java
+++ problems/2610-closest-prime-numbers-in-range/solution.java
@@ -1,42 +1,58 @@
 
 class Solution {
-    public int[] closestPrimes(int left, int right) {
+    
 
-        if (left == 1) {
-            left = left + 1;
-        }
-
-        List<Integer> list = new ArrayList<>();
-        for (int i = left; i <= right; i++) {
-            if (isPrime(i)) {
-                list.add(i);
-            }
-        }
-
-        int minIdx = -1;
-        int min = right + 1;
-
-        if (list.size() <= 1) {
-            return new int[]{-1, -1};
-        }
-
-        for (int i = 1; i < list.size(); i++) {
-            if (list.get(i) - list.get(i - 1) < min) {
-                minIdx = i;
-                min = list.get(i) - list.get(i - 1);
-            }
-        }
-        return new int[]{list.get(minIdx - 1), list.get(minIdx)};
-        
+    public Solution() {
     }
 
-    private boolean isPrime(int num) {
-        int end = (int) Math.sqrt(num);
-        for (int i = 2; i <= end; i++) {
-            if (num % i == 0) {
-                return false;
+    public int[] closestPrimes(int left, int right) {
+
+        boolean[] primes = new boolean[right + 1];
+        Arrays.fill(primes, true);
+        findAllPrimesUntilN(primes);
+
+
+        int prev = left, prev_min_idx = -1;
+        while (prev <= right && !primes[prev]) prev++;
+        int curr = prev + 1, curr_min_idx = -1;
+        int min  = right + 1;
+        while (curr <= right) {
+            while (curr <= right && !primes[curr]) curr++;
+            if (curr <= right && curr - prev < min) {
+                min = curr - prev;
+                prev_min_idx = prev;
+                curr_min_idx = curr;
+            }
+            prev = curr;
+            curr++;
+        }
+
+        return new int[] {prev_min_idx, curr_min_idx};
+    }
+
+    private void findAllPrimesUntilN(boolean[] primes) {
+        // mark 0, 1 as non primes;
+        primes[0] = primes[1] = false;
+
+        for (int i = 2; i <= Math.sqrt(primes.length);) {
+            if (!primes[i]) continue;
+            // Mark all the multiples of i as non primes
+            for (int j = i*i; j > 0 && j < primes.length; j += i) {
+                primes[j] = false;
+            }
+
+            // Find next prime and repeat
+            boolean nextPrimeExists = false;
+            for (int j = i + 1; j < primes.length; j++) {
+                if (primes[j]) {
+                    i = j;
+                    nextPrimeExists = true;
+                    break;
+                }
+            }
+            if (!nextPrimeExists) {
+                break;
             }
         }
-        return true;
     }
 }
