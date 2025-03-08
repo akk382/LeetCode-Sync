3630361f983b19d75c1061f581c072d241a6bf65
Arun <arunyoyo00@gmail.com>
2025-03-08 01:59:17 +0000

    [LeetCode Sync] Runtime - 1 ms (92.10%), Memory - 41.3 MB (86.78%)

--- problems/2463-minimum-recolors-to-get-k-consecutive-black-blocks/solution.java
+++ problems/2463-minimum-recolors-to-get-k-consecutive-black-blocks/solution.java
@@ -1,19 +1,21 @@
 class Solution {
     public int minimumRecolors(String blocks, int k) {
 
-        int[] wCounter = new int[blocks.length()];
-        wCounter[0] = blocks.charAt(0) == 'W' ? 1 : 0;
-        for (int i = 1; i < blocks.length(); i++) {
-            wCounter[i] = blocks.charAt(i) == 'W' ? wCounter[i-1] + 1 : wCounter[i - 1];
-        }
+        int numWhites = 0;
+        int minNeeded = k + 1; 
 
-        int i = k - 1;  // We start from the k - 1 th position.
-        int minNeeded = wCounter[i++];
-        while (i < blocks.length()) {
-            if (wCounter[i] - wCounter[i - k] < minNeeded) {
-                minNeeded = wCounter[i] - wCounter[i - k];
+        for (int i = 0; i < blocks.length(); i++) {
+            if (blocks.charAt(i) == 'W') {
+                numWhites++;    
             }
-            i++;
+
+            if (i >= k && blocks.charAt(i - k) == 'W') {
+                numWhites--;    
+            }
+
+            if (i >= k - 1) {
+                minNeeded = Math.min(minNeeded, numWhites);
+            }
         }
 
         return minNeeded;
