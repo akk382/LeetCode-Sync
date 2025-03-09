7efebc7a456320dd51af8bb6ccbabcbaaadf6bb0
Arun <arunyoyo00@gmail.com>
2025-03-09 01:09:06 +0000

    [LeetCode Sync] Runtime - 6 ms (30.45%), Memory - 61.2 MB (79.05%)

--- problems/3483-alternating-groups-ii/solution.java
+++ problems/3483-alternating-groups-ii/solution.java
@@ -1,34 +1,17 @@
 class Solution {
     public int numberOfAlternatingGroups(int[] colors, int k) {
 
-        // prepare an array of colors such that the first k - 1 colors are appended at the end
-        // If given colors are 0100101, and k = 6. Then prepare an array and append the first k - 1 = 5 colors at the end
-        // Like: 0100101 01001.
-        // We do this only if the first and last of the given colors are different.
-
+        int length = colors.length;
         if (colors[0] != colors[colors.length - 1]) {
-            int[] tempColors = new int[colors.length + k - 1];
-            int i = 0;
-            // Copy all the eleements from the colors to tempColors
-            for (; i < colors.length; i++) {
-                tempColors[i] = colors[i];
-            }
-
-            // Copy the first k - 1 elements from the colors to last k - 1 positions of tempColors
-            while (i < tempColors.length) {
-                tempColors[i] = colors[i % colors.length];
-                i++;
-            }
-
-            colors = tempColors;
+            length = colors.length + k - 1;
         }
 
-        long[] counterArray = new long[colors.length];
+        long[] counterArray = new long[length];
         counterArray[0] = 0L;
 
         // Iterate through the colors until the end;
-        for (int i = 1; i < colors.length; i++) {
-            if (colors[i - 1] != colors[i]) {
+        for (int i = 1; i < length; i++) {
+            if (colors[(i - 1) % colors.length] != colors[i % colors.length]) {
                 counterArray[i] = counterArray[i - 1] + 1;
             } else {
                 counterArray[i] = 0;
@@ -43,7 +26,6 @@ class Solution {
             }
         }
 
-        return groupCounter;
-        
+        return groupCounter;        
     }
 }
