48f92b8b2dc46272d5a72799e4dacb2fb57c90df
Arun <arunyoyo00@gmail.com>
2025-03-11 01:11:24 +0000

    [LeetCode Sync] Runtime - 12 ms (72.97%), Memory - 44.7 MB (64.19%)

--- problems/1460-number-of-substrings-containing-all-three-characters/solution.java
+++ problems/1460-number-of-substrings-containing-all-three-characters/solution.java
@@ -3,20 +3,20 @@ class Solution {
     private int[] abcMap = new int[3];
     
     public int numberOfSubstrings(String s) {
+        char[] charArr = s.toCharArray();
         int resultCount = 0;
-        int left = 0, right = 0;
-        for (; right < s.length(); right++) {
-            char currChar = s.charAt(right);
+        for (int right = 0, left = 0; right < charArr.length; right++) {
+            char currChar = charArr[right];
             int charCount = abcMap[currChar - 'a'];
             abcMap[currChar - 'a'] = charCount + 1;
 
 
             if (allCharsPresent()) {
-                resultCount += s.length() - right;
+                resultCount += charArr.length - right;
 
                 do {
                     left = left + 1;
-                    char reducedChar = s.charAt(left - 1);
+                    char reducedChar = charArr[left - 1];
                     int reducedCharCount = abcMap[reducedChar - 'a'];
                     if (reducedCharCount == 1) {
                         abcMap[reducedChar - 'a'] = 0;
@@ -25,7 +25,7 @@ class Solution {
                     }
 
                     if (allCharsPresent()) {
-                        resultCount += s.length() - right;
+                        resultCount += charArr.length - right;
                     }
                 } while (allCharsPresent());
             }
